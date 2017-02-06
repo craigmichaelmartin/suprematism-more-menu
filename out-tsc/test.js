566c4143dc0ddd9226f9e117495ebd24ffffb7db
Akers.dao <Akers@Railroad19.com>
2017-02-06 08:33:51 -0500

    Fix casing issue (#5)
    Change Observable to observable.  This resolved the warning message, "There are multiple modules with names that only differ in casing."

--- out-tsc/test.js
+++ out-tsc/test.js
@@ -19,4 +19,4 @@ Promise.all([
     .then(function () { return require.context('./', true, /\.spec\.ts/); })
     .then(function (context) { return context.keys().map(context); })
     .then(__karma__.start, __karma__.error);
-//# sourceMappingURL=/Users/craigmartin/projects/suprematism-more-menu/src/test.js.map
+//# sourceMappingURL=/Users/falosakers/Documents/Projects/suprematism-more-menu/src/test.js.map
